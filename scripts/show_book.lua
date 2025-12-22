3a436d5fa59550b083828af84c9714eddaa383b4
marco-vidali <marcovidali@outlook.com>
2025-12-22 20:13:05 +0100

    Change colors

--- scripts/show_book.lua
+++ scripts/show_book.lua
@@ -41,18 +41,18 @@ function _draw()
 
     print("dAY " .. day, 0, 0, 7)
 
-    print("aWARENESS: " .. awareness .. "%", 0, 12, 7)
-    print("hAPPINESS: " .. happiness .. "%", 0, 18, 7)
+    print("aWARENESS: " .. awareness .. "%", 0, 12, 14)
+    print("hAPPINESS: " .. happiness .. "%", 0, 18, 10)
 
-    print_wrapped(book.title, 0, 30, 7, 128)
-    print_wrapped(book.author, 0, 36, 7, 128)
+    print_wrapped(book.title, 0, 30, 12, 128)
+    print_wrapped(book.author, 0, 36, 9, 128)
     print_wrapped(book.synopsis, 0, 42, 7, 128)
 
     if choice == 1 then
-        print("pass", 0, 72, 7)
+        print("pass", 0, 72, 11)
         print("censor", 30, 72, 5)
     elseif choice == 2 then
         print("pass", 0, 72, 5)
-        print("censor", 30, 72, 7)
+        print("censor", 30, 72, 8)
     end
 end
